a6ecc5e8a85cca8d12ed78e73b088912684ba022
Akshay Asok <akshayasokson@gmail.com>
2025-07-01 21:35:26 +0530

    app implementation completed

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { useState } from 'react';
+
+const dict = [
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [definition, setDefinition] = useState("");
+
+  const handleSearch = () => {
+
+    const word = dict.find((item) => item.word.toLowerCase() === search.toLowerCase());
+    // console.log("word >> ", word);
+    if(word){
+      setDefinition(word.meaning);
+    }
+    else{
+      setDefinition("Word not found in the dictionary.")
+    }
+  }
+
   return (
     <div className="App">
-      XDictionary
+      
+      <h1>Dictionary App</h1>
+
+      <input placeholder='Search for a word...' 
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+      onClick={handleSearch}
+      >Search</button>
+      <h3>Definition:</h3>
+      <p>{definition}</p>
     </div>
   );
 }
